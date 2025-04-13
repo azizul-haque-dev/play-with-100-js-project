70209f43eefa1d3ae8e11d96df5c0fb45cf0d671
user-azizul <azizul.contact.me@gmail.com>
2025-04-14 00:28:28 +0300

    Refactor grocery list app: improve variable naming, enhance item editing functionality, and update styles for better UI consistency

--- 008.grocery-list/app.js
+++ 008.grocery-list/app.js
@@ -1,80 +1,106 @@
-const listContainer = document.querySelector(".list-container");
-const btn = document.querySelector(".addbtn");
+const groceryListContainer = document.querySelector(".list-container");
+const addButton = document.querySelector("input");
 
-const input = document.querySelector("input");
+const inputField = document.querySelector("input");
 const form = document.querySelector("form");
-let isEdit = false;
-let editingId;
+let isEditing = false;
+let editingItemId;
+let groceryItems = JSON.parse(localStorage.getItem("groceryItems")) || [];
+let todos = JSON.parse(localStorage.getItem("todos")) || [];
+
+// get list from localStorage and load to dom
+window.addEventListener("DOMContentLoaded", () => {
+  groceryItems.forEach((item) => createGroceryItemElement(item));
+});
+// add cursor on input after loded
+inputField.focus();
 
 form.addEventListener("submit", function (e) {
   e.preventDefault();
-  const value = input.value;
-  if (value && !isEdit) {
-    addList(value);
-    input.value = "";
-  } else if (value && isEdit) {
-    editing();
+  const value = inputField.value;
+
+  // when is editing is flase add the item and if true then edit the item
+  if (value && !isEditing) {
+    addGroceryItem(value);
+    inputField.value = "";
+  } else if (value && isEditing) {
+    updateGroceryItem(value);
   } else {
+    alert("Please enter an item!");
   }
+  // come back to default state
+  resetState();
 });
 
-function addList(value) {
-  const id = new Date().getTime().toString(); // unique id
-  console.log(id);
-  let element = document.createElement("ul"); // create ul element
-  element.classList.add("list"); // add class name to element
+// add item
+function addGroceryItem(value) {
+  const id = new Date().getTime().toString(); // Unique ID for each grocery item
+  const groceryItem = { id, value };
+  groceryItems.push(groceryItem);
+  saveGroceryItemsToLocalStorage();
+  createGroceryItemElement(groceryItem);
+}
 
-  const attr = document.createAttribute("data-id"); // create attribute
-  attr.value = id; //assign the value of attribute
-  element.setAttributeNode(attr);
+// create grocery item element
+function createGroceryItemElement(item) {
+  let element = document.createElement("ul");
+  element.classList.add("grocery-item");
 
-  element.innerHTML = ` 
-    <li> ${value}</li>
-          <div class="btn-container">
-            <button class="edit"><i class="fi fi-rr-edit"></i></button>
-            <button class="delete"><i class="fi fi-rr-trash"></i></button>
-          </div>
-    
-    `;
-  listContainer.appendChild(element);
-  const editBtn = element.querySelector(".edit");
-  const deleteBtn = element.querySelector(".delete");
-  editBtn.addEventListener("click", function (e) {
-    isEdit = true;
-    btn.textContent = "Edit";
+  const itemIdAttr = document.createAttribute("data-id");
+  itemIdAttr.value = item.id;
+  element.setAttributeNode(itemIdAttr);
+
+  element.innerHTML = `
+    <li> ${item.value}</li>
+    <div class="btn-container">
+      <button class="edit-button"><i class="fi fi-rr-edit"></i></button>
+      <button class="delete-button"><i class="fi fi-rr-trash"></i></button>
+    </div>
+  `;
+
+  // append the element to the grocery container
+  groceryListContainer.appendChild(element);
+
+  // delete event listener
+  element
+    .querySelector(".delete-button")
+    .addEventListener("click", function (e) {
+      groceryItems = groceryItems.filter((grocery) => grocery.id !== item.id);
+      saveGroceryItemsToLocalStorage();
+      const target = e.target.closest("ul");
+      groceryListContainer.removeChild(target);
+    });
+  // edit event listener
+  element.querySelector(".edit-button").addEventListener("click", function (e) {
+    isEditing = true;
+    addButton.textContent = "Edit";
+    inputField.focus();
     let list = e.target.parentElement.parentElement.previousElementSibling;
-    editingId = Number(
-      e.target.parentElement.parentElement.parentElement.dataset.id
-    );
-    console.log(editing);
 
-    input.value = list.textContent;
+    editingItemId = e.target.closest("ul").dataset.id;
+    // getting the edit list text vlaue to input
+    inputField.value = list.textContent;
   });
 }
 
-// editing function
-
-function editing() {
-  const value = input.value;
-  const todo = document.querySelector(`ul[data-id='${editingId}']`);
-  todo.querySelector("li").innerHTML = value;
-  defaultState();
+// edit functionality
+function updateGroceryItem(updatedValue) {
+  groceryItems = groceryItems.map((item) => {
+    return item.id === editingItemId ? { ...item, value: updatedValue } : item;
+  });
+  saveGroceryItemsToLocalStorage();
+  const groceryItem = document.querySelector(`ul[data-id='${editingItemId}']`);
+  if (groceryItem) groceryItem.querySelector("li").innerHTML = updatedValue;
 }
 
-function defaultState() {
-  isEdit = false;
-  btn.textContent = "Add";
-  input.value = "";
+// set item to localStorage
+function saveGroceryItemsToLocalStorage() {
+  localStorage.setItem("groceryItems", JSON.stringify(groceryItems));
 }
 
-function giveAlert(className, msg) {
-  let element = document.createElement("p");
-  element.classList.add(className);
-  element.innerHTML = msg;
-  alertParent.appendChild(element);
+// set back default
+function resetState() {
+  isEditing = false;
+  addButton.textContent = "Add Item";
+  inputField.value = "";
 }
-
-// deleteBtn.addEventListener("click", function (e) {
-//     const target = e.target.closest("ul");
-//     listContainer.removeChild(target);
-//   });
