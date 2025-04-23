82c52972d54354074d77566a9d4e5aba5ad56730
user-azizul <azizul.contact.me@gmail.com>
2025-04-24 00:12:45 +0300

    improve the logind of js and ui

--- 016.Random-number-finding/app.js
+++ 016.Random-number-finding/app.js
@@ -1,7 +1,9 @@
 const input = document.querySelector("input");
 const alertBox = document.querySelector(".alert");
+const form = document.querySelector("form");
 
 let tryCount = 0;
+let isBackSpace = false;
 
 function alertMsg(cls, msg) {
   alertBox.innerHTML = `<p class="${cls}">${msg}</p>`;
@@ -10,15 +12,19 @@ function alertMsg(cls, msg) {
 const randomNum = () => Math.floor(Math.random() * 10) + 1;
 let res = randomNum();
 
-input.addEventListener("input", function () {
-  const val = Number(input.value);
+form.addEventListener("submit", function (e) {
+  e.preventDefault();
+  let val = Number(input.value);
+  console.log(val);
 
   tryCount++;
 
   if (val < res) {
-    alertMsg("danger", "Value is too low");
+    alertMsg("danger", `${val} is too low`);
+    form.reset();
   } else if (val > res) {
-    alertMsg("danger", "Value is too high");
+    alertMsg("danger", `${val} is too high`);
+    form.reset();
   } else {
     alertMsg("success", `You got the value on try #${tryCount}`);
   }
